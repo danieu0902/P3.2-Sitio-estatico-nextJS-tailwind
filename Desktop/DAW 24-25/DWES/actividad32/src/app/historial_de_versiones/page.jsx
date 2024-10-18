e50cde8dc942830d0b5f6fb74e6d6a4df35e4698
Daniel E. Serrano Portillo <danieu275@gmail.com>
2024-10-18 19:20:53 +0200

    ...

---
+++ Desktop/DAW 24-25/DWES/actividad32/src/app/historial_de_versiones/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+export const metadata = {
+  title: "Informacion general: convalidaciones, exenciones y títulos",
+};
+export default function Home() {
+  return (
+   <div>Pagina principal</div>
+  );
+}
